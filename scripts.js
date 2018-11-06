2afc8e7a5e862943860ad7c7592b17df9d7104b8
soa20 <soa20@hi.is>
2018-11-06 19:26:29 +0100

    fixed things
    took out comments, fixed edit and white-space issue when adding items

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
 const ENTER_KEYCODE = 13;
 
-/* this eventlistener waits for the DOM to load (all the content in the browser) before it initializes the application.
-We dont want things happen in the javascript before the content is loaded because then youll get undefined objects/errors */
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.form'); /* defining global variables */
   const items = document.querySelector('.items');
-  /* initializes tha application, text is the variable object and its value is a function */
   text.init(form, items);
 });
-/* this defines the application,  */
+
 const text = (() => {
-  let itemText, itemList, items; /*local scope to the variable text only and children */
+  let itemText, itemList, items, newText; /*local scope to the variable text only and children */
 
   function init(_form, _items) {
     items = _items; /* assigning value to items */
@@ -26,68 +23,65 @@ const text = (() => {
       itemList[i].querySelector('.item__button').addEventListener('click', deleteItem);
     }
   }
-  /* formhandler validates to make sure the form is not empty and calls to add function to add to-do items */
+
   function formHandler(e) {
+    e.preventDefault();
     let isValid = false;
     let newToDo = document.querySelector('.form__input');
-    e.preventDefault();
-    if (newToDo.value != "") {
-      console.log(newToDo.value);
+    newToDo.value = newToDo.value.trim();
+    if (newToDo.value.length > 0) {
       isValid = true;
       add(newToDo.value);
       newToDo.value = "";
     } else {
-      alert("Bættu við verkefni fyrir lista");
+      return;
     }
     return console.log("Form Submitted", isValid);
   }
 
-  // event handler fyrir það að klára færslu
   function finish(e) {
     e.target.parentElement.classList.toggle("item--done");
     return console.log("Finish Me", e);
   }
 
-  // event handler fyrir það að breyta færslu
   function edit(e) {
     let container = e.target.parentElement;
     let theText = e.target.textContent;
     let editable;
-    if (e.target.parentElement.classList.contains('item--done') === true) {
-      return;
-    }
     editable = el("input", "item__edit", false);
-    editable.addEventListener("blur", commit);
     editable.value = theText;
+    editable.addEventListener("blur", commit);
+    editable.addEventListener("input", function onInput(e) {
+      text.newText = e.target.value.trim();
+      console.log("onChange", text.newText);
+    }, true);
     container.insertBefore(editable, e.target.nextSibling);
-    e.target.remove();
     editable.focus();
+
+    e.target.setAttribute("style", "display:none;");
+
     editable.addEventListener("keypress", function onPress(e) {
       if (e.keyCode === ENTER_KEYCODE) {
-        console.log("Enter Key Pressed While Editing");
+        let theSpan = e.target.parentElement.querySelector(".item__text");
+        if (text.newText !== undefined && text.newText.length > 0) {
+          theSpan.textContent = text.newText;
+        }
+        console.log("Enter Key Pressed While Editing", theText, text.newText.length);
         return editable.blur();
       }
     });
-    console.log("Edit Me", e, container, theText);
+    return console.log("Edit Me", e);
   }
 
-  // event handler fyrir það að klára að breyta færslu
   function commit(e) {
     let container = e.target.parentElement;
-    let theText = e.target.value;
-    let notEditable;
-    if (theText === "") {
-      e.target.focus();
-      return;
-    };
-    notEditable = el("span", "item__text", edit);
-    notEditable.textContent = theText;
-    container.insertBefore(notEditable, e.target.nextSibling);
+    let notEditable = e.target.parentElement.querySelector(".item__text");
+    notEditable.setAttribute("style", "display:inline;");
     e.target.remove();
-    console.log("Commit Me", e);
+    text.newText = undefined;
+    return console.log("Commit Me", e);
   }
 
-  /* creating and formatting a new to-do item and inserts it into the DOM */
   function add(value) {
     let newListItem,
       newInput,
@@ -115,29 +109,34 @@ const text = (() => {
     return console.log("Delete Me", e);
   }
 
-  /* Creates a single DOM element */
   function el(type, className, clickHandler) {
     const newEl = document.createElement(type);
     let isCreated = false;
-    if (typeof type !== "undefined") {
-      if (type === "input" && className === "item__checkbox") {
-        newEl.setAttribute('type', 'checkbox');
-      } else if (type === "input" && className === "item__edit") {
-        newEl.setAttribute('type', 'text');
+
+    try {
+      if (typeof type !== "undefined") {
+        if (type === "input" && className === "item__checkbox") {
+          newEl.setAttribute('type', 'checkbox');
+        } else if (type === "input" && className === "item__edit") {
+          newEl.setAttribute('type', 'text');
+        }
+        if (typeof className !== "undefined") {
+          newEl.setAttribute('class', className);
+        }
+        if (typeof clickHandler !== "undefined" && clickHandler) {
+          newEl.addEventListener('click', clickHandler);
+        }
+        isCreated = true;
       }
-      if (typeof className !== "undefined") {
-        newEl.setAttribute('class', className);
-      }
-      if (typeof clickHandler !== "undefined" && clickHandler) {
-        newEl.addEventListener('click', clickHandler);
-      }
-      isCreated = true;
+
+    } catch (e) {
+      alert("ERROR:ElementCreation - The TODO list item was not created.");
+      console.log("ERROR: function el()", e);
     }
 
-    console.log("Create Me", type, className, clickHandler);
+    console.log("CREATE ELEMENT:", isCreated, type, className, clickHandler);
     return newEl;
   }
-
   return {
     init: init
   }
